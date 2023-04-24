18bd6b7c129c685e911c9470bae89c16a49fd57c
norbertsaja <sajanorbert74@icloud.com>
2023-04-24 12:48:43 +0200

    Szkielet ekranu gry z kilkoma nowosciami

--- src/main/java/com/example/hangman_ver2/HelloController.java
+++ src/main/java/com/example/hangman_ver2/HelloController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloController {
 
     @FXML
@@ -13,19 +16,25 @@ public class HelloController {
     private TextField letterField;
     @FXML
     private Button checkButton;
+    @FXML
+    private Button startButton;
     private final String password;
     private StringBuilder hiddenPassword = new StringBuilder();
     private String hiddenPass;
     private boolean importPassword = false;
+    private List<Character> containsLetter = new ArrayList<>();
     public HelloController(){
         PasswordSetter passwordSetter = new PasswordSetter();
         password = passwordSetter.getPassword();
+
+        containsLetter.forEach((n) -> containsLetter.add(password.charAt(n)));
+
         if(password.equals("ERROR:001")){
             passwordLabel.setText("Nie mozna zaladowac hasla");
             System.out.println("ERROR 001");
         }
         else {
-            hiddenPassword.append("_".repeat(password.length()));
+            hiddenPassword.append("_ ".repeat(password.length()));
             hiddenPass = String.valueOf(hiddenPassword);
             System.out.println(hiddenPass);
             System.out.println(hiddenPassword);
@@ -40,6 +49,19 @@ public class HelloController {
         }
         else {
             passwordLabel.setText("Error");
+        }
+        startButton.setVisible(false);
+        letterField.setEditable(true);
+        checkButton.setVisible(true);
+    }
+    public void checkLetter(){
+        char x = letterField.getText().toUpperCase().charAt(0);
+        System.out.println(x);
+        if (containsLetter.contains(x)){
+            for(int i = 0 ; i < hiddenPassword.length(); i++){
+
+            }
+
         }
     }
 
