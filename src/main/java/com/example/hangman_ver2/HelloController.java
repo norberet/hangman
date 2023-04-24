3a7c74f1fb107a7565f652e03f570f544340cd8a
norbertsaja <sajanorbert74@icloud.com>
2023-04-24 11:29:12 +0200

    Szkielet ekranu gry

---
+++ src/main/java/com/example/hangman_ver2/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.hangman_ver2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    public HelloController(){
+
+    }
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private TextField letterField;
+    @FXML
+    private Button checkButton;
+
+    public void setPasswordLabel{
+
+    }
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.hangman_ver2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.hangman_ver2 to javafx.fxml;
+    exports com.example.hangman_ver2;
+}
