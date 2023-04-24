b8de9685872166546b354ca0e0a36576382a9b52
norbertsaja <sajanorbert74@icloud.com>
2023-04-24 13:20:42 +0200

    dzialajaca gra-prawie

--- src/main/java/com/example/hangman_ver2/HelloController.java
+++ src/main/java/com/example/hangman_ver2/HelloController.java
@@ -22,12 +22,17 @@ public class HelloController {
     private StringBuilder hiddenPassword = new StringBuilder();
     private String hiddenPass;
     private boolean importPassword = false;
-    private List<Character> containsLetter = new ArrayList<>();
+    private StringBuilder visiblePassword = new StringBuilder();
+    final int maxBad = 5;
+    final int maxGood;
+    int good = 0;
+    int bad = 0;
     public HelloController(){
         PasswordSetter passwordSetter = new PasswordSetter();
         password = passwordSetter.getPassword();
+        maxGood = password.length();
 
-        containsLetter.forEach((n) -> containsLetter.add(password.charAt(n)));
+
 
         if(password.equals("ERROR:001")){
             passwordLabel.setText("Nie mozna zaladowac hasla");
@@ -35,9 +40,13 @@ public class HelloController {
         }
         else {
             hiddenPassword.append("_ ".repeat(password.length()));
+            for(int i = 0; i < password.length(); i++){
+                visiblePassword.append(password.charAt(i) + " ");
+            }
+
             hiddenPass = String.valueOf(hiddenPassword);
-            System.out.println(hiddenPass);
             System.out.println(hiddenPassword);
+            System.out.println(visiblePassword);
             importPassword = true;
 
         }
@@ -57,11 +66,33 @@ public class HelloController {
     public void checkLetter(){
         char x = letterField.getText().toUpperCase().charAt(0);
         System.out.println(x);
-        if (containsLetter.contains(x)){
-            for(int i = 0 ; i < hiddenPassword.length(); i++){
-
+        String a = Character.toString(x);
+        System.out.println(password.contains(a));
+        if(password.contains(a)){
+            for(int i = 0; i < hiddenPassword.length() ; i++){
+                if(x == visiblePassword.charAt(i)){
+                    hiddenPassword.deleteCharAt(i);
+                    hiddenPassword.insert(i, x);
+                    good++;
+                }
             }
+        }
+        else
+            bad++;
 
+        passwordLabel.setText(String.valueOf(hiddenPassword));
+        letterField.clear();
+        if(good >= maxGood){
+            System.out.println("Wygrana");
+            letterField.setEditable(false);
+            checkButton.setVisible(false);
+            passwordLabel.setText("Wygrana! Hasło to: " + password);
+        }
+        else if(bad >= maxBad){
+            System.out.println("Przegrana");
+            letterField.setEditable(false);
+            checkButton.setVisible(false);
+            passwordLabel.setText("Przegrana! Hasło to: " + password);
         }
     }
 
