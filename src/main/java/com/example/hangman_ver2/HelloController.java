5f7c161c81d6db87815cb8131500ce09c277fa94
norbertsaja <sajanorbert74@icloud.com>
2023-04-25 15:47:25 +0200

    update

--- src/main/java/com/example/hangman_ver2/HelloController.java
+++ src/main/java/com/example/hangman_ver2/HelloController.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.*;
+
 
 public class HelloController {
 
@@ -24,6 +26,7 @@ public class HelloController {
     private String hiddenPass;
     private boolean importPassword = false;
     private StringBuilder visiblePassword = new StringBuilder();
+    private List<Character> usedLetter = new ArrayList<>();
     private final int maxBad = 5;
     private final int maxGood;
     private int good = 0;
@@ -58,8 +61,20 @@ public class HelloController {
                 //jesli haslo sklada sie z jednego wyrazu to wszystko uzupelniamy znakiem '_' ze spacja
             }
 
-            for(int i = 0; i < password.length(); i++){
-                visiblePassword.append(password.charAt(i)).append(" ");
+            if(password.contains(" ") || password.contains("-")){ //to samo dla widocznego hasla
+                for (int i = 0; i < password.length(); i++) {
+                    if (password.charAt(i) == ' ') {
+                        visiblePassword.append("  ");
+                    } else if (password.charAt(i) == '-') {
+                        visiblePassword.append("- ");
+                    } else {
+                        visiblePassword.append(password.charAt(i)).append(" ");
+                    }
+                }
+            }else {
+                for (int i = 0; i < password.length(); i++) {
+                    visiblePassword.append(password.charAt(i)).append(" ");
+                }
             }
 
             hiddenPass = String.valueOf(hiddenPassword);
@@ -82,23 +97,28 @@ public class HelloController {
         checkButton.setVisible(true);
     }
     public void checkLetter(){
-        char x = letterField.getText().toUpperCase().charAt(0);
-        System.out.println(x);
-        String a = Character.toString(x);
-        System.out.println(password.contains(a));
-        if(password.contains(a)){
-            for(int i = 0; i < hiddenPassword.length() ; i++){
-                if(x == visiblePassword.charAt(i)){
-                    hiddenPassword.deleteCharAt(i);
-                    hiddenPassword.insert(i, x);
-                    good++;
-                }
-            }
+        char x = letterField.getText().toUpperCase().charAt(0); //pobieramy litere z pola i ustawiamy zmieniamy na wielka
+        if(usedLetter.contains(x)){
+            System.out.println("Już podałeś te litere"); //jesli ta litera juz sie pojawila to nie program nie idzie dalej
         }
-        else
-            bad++;
-
-        passwordLabel.setText(String.valueOf(hiddenPassword));
+        else {
+            usedLetter.add(x);
+            System.out.println(x);
+            String a = Character.toString(x);
+            System.out.println(password.contains(a));
+            if (password.contains(a)) {
+                for (int i = 0; i < hiddenPassword.length(); i++) {
+                    if (x == visiblePassword.charAt(i)) {
+                        hiddenPassword.deleteCharAt(i);
+                        hiddenPassword.insert(i, x);
+                        good++;
+                    }
+                }
+            } else {
+                bad++;
+            }
+            passwordLabel.setText(String.valueOf(hiddenPassword));
+        }
         letterField.clear();
         if(good >= maxGood){
             System.out.println("Wygrana");
